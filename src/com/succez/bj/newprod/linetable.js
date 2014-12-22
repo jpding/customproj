c7984c5089ebca40d6de9fac7ce2d7a5e29d7d9e
jpding <jpding@gmail.com>
2014-12-22 15:19:17 +0800

    船总线表显示

--- src/com/succez/bj/newprod/linetable.js
+++ src/com/succez/bj/newprod/linetable.js
@@ -1,3 +1,12 @@
+var TABLE_NAME = "rpt1";
+var COL_EVENT_TIME_SRC = "H";
+var COL_EVENT_KEY_SRC  = "I";
+var COL_STARTTIME = "F";
+var COL_ENDTIME   = "G";
+var COL_STATE     = "J";
+var ROW_START     = 3; // 0 base
+
+
 /**
  * 报表计算后脚本
  * args.report
@@ -6,45 +15,86 @@
  */
 function onAfterReportCalc(args) {
     var result = args.result;
-    var table = result.getComponent("rpt1");
-    var startCol = 17;
-    for(var i=0, len=table.getRowCount(); i<len; i++ ){
+    var table = result.getComponent(TABLE_NAME);
+    var startCol = getStartCol(table);
+    if(startCol == -1){
+    	println("startCol:start col error !");
+    	return ;
+    }
+    
+    var startTimeCol = getColIndex(COL_STARTTIME)-1;
+    var endTimeCol   = getColIndex(COL_ENDTIME)-1;
+    
+    var dataStartCol = startCol+1;
+    var rows = table.getRows();
+    for(var i=ROW_START, len=table.getRowCount(); i<len; i++ ){
+    	var visible = checkVisible(table, i,  endTimeCol+1, startCol);
+    	if(!visible){
+    		rows[i].setVisible(0);
+    		continue;
+    	}
+    	
     	var cell = table.getCell(i, startCol);
     	var vv = cell.value;
     	if(vv == '计划'){
     		if(i%2==0){
-    			var startCell = getStartCell(table, cell);
-    			var endCell   = getEndCell(table, cell);
+    			var startCell = getStartCell(table, cell, dataStartCol);
+    			var endCell   = getEndCell(table, cell, dataStartCol);
     			if(startCell == null || endCell == null){
     				continue;
     			}
-    			drawPlanLine(table, startCell, endCell);
-    			drawMileStone(table, startCell, endCell);
+    			drawPlanLine(table, startCell, endCell, dataStartCol);
+    			drawMileStone(table, startCell, endCell, dataStartCol, startTimeCol, endTimeCol, startCol);
     		}
     	}else if(vv == '执行'){
     		if(i%2==0){
-    			var startCell = getStartCell(table, cell);
-    			var endCell   = getEndCell(table, cell);
+    			var startCell = getStartCell(table, cell, dataStartCol);
+    			var endCell   = getEndCell(table, cell, dataStartCol);
     			if(startCell == null || endCell == null){
     				continue;
     			}
-    			drawPlanLine2(table, startCell, endCell);
-    			drawMileStone(table, startCell, endCell);
+    			drawPlanLine2(table, startCell, endCell, dataStartCol);
+    			drawMileStone(table, startCell, endCell, dataStartCol, startTimeCol, endTimeCol, startCol);
     		}
     	}
     }
 }
 
-function getStartCell(table, cell){
-	var cl = table.getCell(cell.row, 5);
-	var vv = cl.value;
-	return getConditionCell(table, cell, vv);
+function checkVisible(table, rowIndex, startCol, endCol){
+	for(var i=startCol; i<endCol; i++){
+		var cell = table.getCell(rowIndex, i);
+		if(cell.value){
+			return true;
+		}
+	}
+	return false;
 }
 
-function getEndCell(table, cell){
-	var cl = table.getCell(cell.row, 6);
+function getStartCol(table){
+	for(var i=0, len=table.getColCount(); i<len; i++){
+		var cell = table.getCell(ROW_START, i);
+		if(cell == null){
+			continue;
+		}
+		if(cell.value == "计划"){
+			return i;
+		}			
+	}	
+	return -1;
+}
+
+function getStartCell(table, cell, dataStartCol){
+	var startcol = getColIndex(COL_STARTTIME) - 1;
+	var cl = table.getCell(cell.row, startcol);
 	var vv = cl.value;
-	return getConditionCell(table, cell, vv);
+	return getConditionCell(table, cell, vv, dataStartCol);
+}
+
+function getEndCell(table, cell, dataStartCol){
+	var endcol = getColIndex(COL_ENDTIME) - 1;
+	var cl = table.getCell(cell.row, endcol);
+	var vv = cl.value;
+	return getConditionCell(table, cell, vv, dataStartCol);
 }
 
 function drawNoBorder(table, startCell, startCol){
@@ -55,8 +105,8 @@ function drawNoBorder(table, startCell, startCol){
 	}
 }
 
-function drawPlanLine(table, startCell, endCell){
-	drawNoBorder(table, startCell, 18);
+function drawPlanLine(table, startCell, endCell, startCol){
+	drawNoBorder(table, startCell, startCol);
 	
 	for(var col=startCell.col;col<=endCell.col; col++){
 		var cl = table.getCell(startCell.row, col);
@@ -65,8 +115,8 @@ function drawPlanLine(table, startCell, endCell){
 	}
 }
 
-function drawPlanLine2(table, startCell, endCell){
-	drawNoBorder(table, startCell, 18);
+function drawPlanLine2(table, startCell, endCell, startCol){
+	drawNoBorder(table, startCell, startCol);
 	
 	for(var col=startCell.col;col<=endCell.col; col++){
 		var cl = table.getCell(startCell.row, col);
@@ -74,45 +124,38 @@ function drawPlanLine2(table, startCell, endCell){
 	}
 }
 
-function drawMileStone(table, startCell, endCell){
-	var startTime = table.getCell(startCell.row, 5);
-	var endTime = table.getCell(startCell.row, 6);
-	var key1 = table.getCell(startCell.row, 7);
-	var key2 = table.getCell(startCell.row, 9);
-	var key3 = table.getCell(startCell.row, 11);
-	var key4 = table.getCell(startCell.row, 13);
-	var key5 = table.getCell(startCell.row, 15);
+function drawMileStone(table, startCell, endCell, dataStartCol, startTimeCol, endTimeCol, startCol){
+	var startTime = table.getCell(startCell.row, startTimeCol);
+	var endTime = table.getCell(startCell.row, endTimeCol);
 	var cells = [];
 	cells.push(startTime);
 	cells.push(endTime);
-	cells.push(key1);
-	cells.push(key2);
-	cells.push(key3);
-	cells.push(key4);
-	cells.push(key5);
+	for(var i=endTimeCol+1; i<startCol; ){
+		var celltime = table.getCell(startCell.row, i);
+		cells.push(celltime);
+		println("celltime:"+celltime.value);
+		i += 2;
+	}
+	
 	for(var i=0; i<cells.length; i++){
 		var v = cells[i].value;
 		if(v==null || v.length == 0){
 			continue;
 		}
 		var vv = v.substring(0,6);
-		var cl = getConditionCell(table, startCell, vv);
+		var cl = getConditionCell(table, startCell, vv, dataStartCol);
 		if(cl != null){
 			cl.value= (i>=2?"★":"")+java.lang.Integer.parseInt(v.substring(4,6))+"."+v.substring(6,8);
 		}
 	}
 	
-	var pkey1 = table.getCell(startCell.row, 8);
-	var pkey2 = table.getCell(startCell.row, 10);
-	var pkey3 = table.getCell(startCell.row, 12);
-	var pkey4 = table.getCell(startCell.row, 14);
-	var pkey5 = table.getCell(startCell.row, 16);
 	var cellContents = [];
-	cellContents.push(pkey1);
-	cellContents.push(pkey2);
-	cellContents.push(pkey3);
-	cellContents.push(pkey4);
-	cellContents.push(pkey5);
+	for(var i=endTimeCol+2; i<startCol; ){
+		var cellKey = table.getCell(startCell.row, i);
+		cellContents.push(cellKey);
+		i += 2;
+	}
+
 	for(var i=0; i<cellContents.length; i++){
 		var v = cellContents[i].value;
 		if(v == null || v.length == 0){
@@ -124,7 +167,7 @@ function drawMileStone(table, startCell, endCell){
 			continue;
 		}
 		var month = rq.substring(0,6);
-		var cl = getConditionCell(table, startCell, month);
+		var cl = getConditionCell(table, startCell, month, dataStartCol);
 		if(cl != null){
 			var resultCell = table.getCell(cl.row+1, cl.col);
 			if(resultCell != null){
@@ -134,20 +177,28 @@ function drawMileStone(table, startCell, endCell){
 	}
 }
 
-function getConditionCell(table, cell, vv){
-	println("==============="+vv)
+function getConditionCell(table, cell, yearMonth, dataStartCol){
+	var vv = yearMonth;
+	println("getConditionCell==============="+vv)
 	if(vv == null || vv.length == 0){
 		return null;
 	}
 	
 	var year = vv.substring(0,4);
-	var month = vv.substring(4,6);
-	for(var i=18; i<table.getColCount(); i++){
-		var cell1 = table.getCell(0,i);
-		var cell2 = table.getCell(1,i);
+	var month = null;
+	if(vv.length < 6){
+		println("getConditionCell:error:"+vv+"\t"+cell.row+"\t"+cell.col);
+		return null;
+	}
+	month = java.lang.Integer.parseInt(vv.substring(4,6));
+	for(var i=dataStartCol; i<table.getColCount(); i++){
+		var cell1 = table.getCell(1,i);
+		var cell2 = table.getCell(2,i);
 		try{
-			println(cell1.value+"\t"+year+"\t"+java.lang.Integer.parseInt(cell2.value)+"\t"+java.lang.Integer.parseInt(month));
-			if(cell1.value == year && java.lang.Integer.parseInt(cell2.value) == java.lang.Integer.parseInt(month)){
+			var smm = java.lang.Integer.parseInt(tostr(cell2.value,"mm"));
+			//println("getConditionCell:"+cell1.value+"\t"+year+"\t"+smm+"\t"+month);
+			if(cell1.value == year && smm == month){
+				println("resultcell:"+year+"\t"+smm);
 				return table.getCell(cell.row, i);
 			}
 		}catch(e){
@@ -155,4 +206,8 @@ function getConditionCell(table, cell, vv){
 		}
 	}
 	return null;
+}
+
+function getColIndex(colName){
+	return colName.charCodeAt(0) - "A".charCodeAt(0)+1; 
 }
