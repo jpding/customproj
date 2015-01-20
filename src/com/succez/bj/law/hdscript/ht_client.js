be1ea555414193f4f92870e1d0d1497f1ef12575
jpding <jpding@gmail.com>
2015-01-20 09:34:39 +0800

    和合同客户端保持一致

--- src/com/succez/bj/law/hdscript/ht_client.js
+++ src/com/succez/bj/law/hdscript/ht_client.js
@@ -1,8 +1,24 @@
 var modelInfo = {
-	3520 : {
+	"384b099f5bf84bcd9780a880e688b467" : {
 	 	 cont_main_type : "0600",
 		  cont_sub_type : "0610",
 		  money_type : "fact"
+	}
+}
+
+function oninitwiform($flow){
+	var fillForm = $flow.getForm();
+	var nodealias = $flow.params.form;
+	fillForm.setValue(nodealias,"nodealias","HZ_CONT_INFO");
+	var formDom = fillForm.basedom();
+
+	if(nodealias == "STARTFORM"){
+		$("#text9",formDom).hide();
+	}else{
+		if("TZWBGS" == nodealias){
+			$("#text9>a",formDom).text("调整");
+		}
+		sz.ci.custom.hzutils.removeTextLink(formDom,["text8","text6","text11","text16","text17","text7","text19"]);
 	}
 }
 
@@ -10,22 +26,65 @@ var modelInfo = {
 * 在起草合同的时候，判断是否使用范本起草的合同，并将范本信息写入到合同信息中
 */
 function oninitwiform_STARTFORM($flow){
-	var utils = sz.utils;
-	var fb_uid = utils.getParameter("fb_uid");
-	var fillforms = $flow.getForm();
-	if (fb_uid && fb_uid != ""){
-		fillforms.setValue(fb_uid,"fb_uid","HZ_CONT_INFO");
-		fillforms.setValue("1","iscontractmodel","HZ_CONT_INFO");
-		
-		var info = modelInfo[fb_uid];
-		if (info){
-			for (var name in info){
-				fillforms.setValue(info[name],name,"HZ_CONT_INFO");
+	if (!$flow.businesskey){
+		var utils = sz.utils;debugger;
+		var fb_uid = utils.getParameter("fb_uid");
+		var sjuid = utils.getParameter("sjuid");
+		var gysdm = utils.getParameter("gysdm");
+		var gcbh = utils.getParameter("gcbh");
+		var fillforms = $flow.getForm();
+		if (fb_uid && fb_uid != ""){
+			fillforms.setValue(fb_uid,"fb_uid","HZ_CONT_INFO");
+			fillforms.setValue("1","iscontractmodel","HZ_CONT_INFO");
+			
+			var info = modelInfo[fb_uid];
+			if (info){
+				for (var name in info){
+					fillforms.setValue(info[name],name,"HZ_CONT_INFO");
+				}
 			}
+			
+			//初始化范本进去
+			if(sz.ci && sz.ci.custom && sz.ci.custom.uploadattachment){
+			   var curForm = $flow.getForm().getCurrentForm();
+			   sz.ci.custom.uploadattachment.uploadAndEditContractByModel(curForm, "htwb",null,true)
+			}
+		}else{
+			fillforms.setValue("0","iscontractmodel","HZ_CONT_INFO");
+		};	
+		
+		//初始化审价id进去
+		if(sjuid){
+			var curForm = $flow.getForm().getCurrentForm();
+			setTimeout(function(){
+				var floatArea = curForm.getFloatArea("table5.b2");
+				var row = floatArea.lastRow();debugger;
+				if(!row.isBlank()){
+					row = floatArea.newRow();
+				}
+				var comp = row.getComponent("table5.b2");
+				comp.val(sjuid);
+			},1000)
+			
+		};
+		
+		if(gysdm){
+			var curForm = $flow.getForm().getCurrentForm();
+			setTimeout(function(){
+				var floatArea = curForm.getFloatArea("table1.b2");
+				var row = floatArea.lastRow();debugger;
+				if(!row.isBlank()){
+					row = floatArea.newRow();
+				}
+				var comp = row.getComponent("table1.b2");
+				comp.val(sjuid);
+			},1000)
+		};
+		
+		if(gcbh){
+			fillforms.setValue(gcbh,"proj_num","HZ_CONT_INFO");
 		}
-	}else{
-		fillforms.setValue("0","iscontractmodel","HZ_CONT_INFO");
-	};
+	}
 
 	$.addCallbacks("save_"+$flow.getForm().getCurrentFormName(), function(){
 		sz.commons.CheckSaved.getInstance().setModified();
@@ -34,11 +93,15 @@ function oninitwiform_STARTFORM($flow){
 			window.parent.navTab.openTab("custom_24",url ,{title:"合同草稿箱", fresh:true, external:true})
 		}
 	});
+	
+	/**
+	 * 似乎只有合同表单切换页面时，提示 数据尚未上报，确实要离开当前页面吗？ 没找出具体原因，先在这里关闭提示
+	 */
+	sz.commons.CheckSaved.getInstance().off();
 } 
 
-
 /**
-** 在“维护“的界面提供”增加“和”删除“按钮
+** 在“维护“的界面去掉”增加“和”删除“按钮
 **/
 function oninitwiquery($flow){
 	var buttons = ['wiadd'];
@@ -53,33 +116,3 @@ function hiddenWIButtons($flow, buttons){
      	}	
 	})
 }
-
-/**
-* 隐藏某些按钮
-*/
-function disableButtons(formDom,removeIDs){
-	if (removeIDs){
-		for (var i=0;i<removeIDs.length;i++){
-			var id = removeIDs[i];
-			var dom = $("#"+id,formDom);
-			var text = dom.text();
-			dom.find("a").remove();
-			dom.text(text).css("color","black").css("text-decoration" , "none");
-		}
-	};
-}
-
-/**
-   在规章制度立项的时候，不显示在线编辑按钮，只允许上传规章制度文本
-*/
-function oninitwiform($flow){
-	var fillForm = $flow.getForm();
-	var formDom = fillForm.basedom();
-	if($flow.form=="STARTFORM"){
-		disableButtons(formDom,["text9"]);
-		$("#text9").hide();
-	}else{
-		disableButtons(formDom,["text8","text6","text11","text16","text17","text7"]);
-	}
-}
-
